59649bf53671bb766b030c27517029953df77b87
Daniel Amuedo <dkanh6@umsystem.edu>
2023-11-18 17:48:52 -0600

    Added more functions to calculate motor paramters dynamically based on user input

--- MotorThermalAnalysis.m
+++ MotorThermalAnalysis.m
@@ -25,7 +25,7 @@ classdef MotorThermalAnalysis
 
         function motorComponents = getUserInput()
             % Ask user to choose between default values or custom values
-            choice = input('Do you want to use default values? (Y/N): ', 's');
+            choice = MotorThermalAnalysis.getYorNInput('Do you want to use default values? (Y/N): ');
 
             if upper(choice) == 'Y'
                 % Use default values
@@ -35,30 +35,61 @@ classdef MotorThermalAnalysis
                 % cp Steel = 0.466 J/gC
                 [cp_stator, k_stator] = MotorThermalAnalysis.calcCompositeProperties(0.5); % 50% copper
                 stator = struct('OuterDiameter', 27.5, 'InnerDiameter', 5, 'Length', 26, 'K_Conductivity', k_stator, 'C_HeatCapacity', cp_stator); % Assuming 50:50 ratio of copper to steel to calculate the thermal capacitance and conductivity
-                rotor = struct('OuterDiameter', 35, 'InnerDiameter', 30, 'Length', 36, 'K_Conductivity', 45, 'C_HeatCapacity', 850);
-                axle = struct('OuterDiameter', 20, 'InnerDiameter', 0, 'Length', 60, 'K_Conductivity', 167, 'C_HeatCapacity', 0.89);
+                rotor = struct('OuterDiameter', 35, 'InnerDiameter', 30, 'Length', 36, 'K_Conductivity', 167, 'C_HeatCapacity', 0.89);
+                axle = struct('OuterDiameter', 5, 'InnerDiameter', 0, 'Length', 55, 'K_Conductivity', 50, 'C_HeatCapacity', 0.466);
             else
                 % Prompt for custom values
-                disp('Please enter the stator information:');
-                stator.OuterDiameter = input('Enter stator outer diameter (mm): ');
-                stator.InnerDiameter = input('Enter stator inner diameter (mm): ');
-                stator.Length = input('Enter stator length (mm): ');
-                stator.K_Conductivity = input('Enter stator thermal conductivity (W/mK): ');
-                stator.C_HeatCapacity = input('Enter stator specific heat capacity (J/kgK): ');
+                disp('==============================');
+                disp('CUSTOM MOTOR COMPONENT INPUT');
+                disp('==============================');
 
-                disp('Please enter the rotor information:');
-                rotor.OuterDiameter = input('Enter rotor outer diameter (mm): ');
-                rotor.InnerDiameter = input('Enter rotor inner diameter (mm): ');
-                rotor.Length = input('Enter rotor length (mm): ');
-                rotor.K_Conductivity = input('Enter rotor thermal conductivity (W/mK): ');
-                rotor.C_HeatCapacity = input('Enter rotor specific heat capacity (J/kgK): ');
+                % Stator Information
+                disp('STATOR INFORMATION:');
+                disp('-------------------');
+                stator.OuterDiameter = input('Enter Stator Outer Diameter (mm): ');
+                stator.InnerDiameter = input('Enter Stator Inner Diameter (mm): ');
+                stator.Length = input('Enter Stator Length (mm): ');
+                stator.K_Conductivity = input('Enter Stator Thermal Conductivity (W/mK): ');
+                stator.C_HeatCapacity = input('Enter Stator Specific Heat Capacity (J/kgK): ');
+                disp(' ');
 
-                disp('Please enter the axle information:');
-                axle.OuterDiameter = input('Enter axle outer diameter (mm): ');
-                axle.InnerDiameter = input('Enter axle inner diameter (mm): ');  % Typically zero
-                axle.Length = input('Enter axle length (mm): ');
-                axle.K_Conductivity = input('Enter axle thermal conductivity (W/mK): ');
-                axle.C_HeatCapacity = input('Enter axle specific heat capacity (J/kgK): ');
+                % Rotor Information
+                disp('ROTOR INFORMATION:');
+                disp('------------------');
+                rotor.OuterDiameter = input('Enter Rotor Outer Diameter (mm): ');
+                rotor.InnerDiameter = input('Enter Rotor Inner Diameter (mm): ');
+                rotor.Length = input('Enter Rotor Length (mm): ');
+                rotor.K_Conductivity = input('Enter Rotor Thermal Conductivity (W/mK): ');
+                rotor.C_HeatCapacity = input('Enter Rotor Specific Heat Capacity (J/kgK): ');
+                disp(' ');
+
+                % Axle Information
+                disp('AXLE INFORMATION:');
+                disp('-----------------');
+
+                % Option to dynamically estimate axle data or enter manually
+                axleChoice = MotorThermalAnalysis.getYorNInput('Do you want to dynamically estimate axle data? (Y/N): ');
+
+
+                if upper(axleChoice) == 'Y'
+                    % Logic to dynamically estimate axle data
+                    % Example: Estimating based on rotor/stator dimensions
+                    axle.OuterDiameter = rotor.InnerDiameter; % Example estimation
+                    axle.InnerDiameter = 0; % Typically zero for a solid axle
+                    axle.Length = stator.Length; % Example estimation
+                    axle.K_Conductivity = 60; % Default or estimated value
+                    axle.C_HeatCapacity = 920; % Default or estimated value
+                else
+                    % Manual input for axle data
+                    axle.OuterDiameter = input('Enter Axle Outer Diameter (mm): ');
+                    axle.InnerDiameter = input('Enter Axle Inner Diameter (mm): '); % Typically zero
+                    axle.Length = input('Enter Axle Length (mm): ');
+                    axle.K_Conductivity = input('Enter Axle Thermal Conductivity (W/mK): ');
+                    axle.C_HeatCapacity = input('Enter Axle Specific Heat Capacity (J/kgK): ');
+                end
+
+                % Construct and return the motorComponents structure
+                motorComponents = struct('stator', stator, 'rotor', rotor, 'axle', axle);
             end
 
         end
@@ -80,7 +111,59 @@ classdef MotorThermalAnalysis
             R_thermal = log(outerRadius/innerRadius) / (2 * pi * k * length);
         end
 
+        function response = getYorNInput(promptMessage)
+            validInput = false;
+            while ~validInput
+                response = input(promptMessage, 's');
+                if strcmpi(response, 'Y') || strcmpi(response, 'N')
+                    validInput = true;
+                else
+                    disp('Invalid input. Please enter Y (Yes) or N (No).');
+                end
+            end
+        end
 
+        function axle = estimateAxleDimensions(stator, rotor)
+            % This logic can be adjusted based on how the axle fits with the rotor and stator
+
+            % Estimate the outer diameter of the axle (could be slightly less than the inner diameter of the rotor)
+            axle.OuterDiameter = rotor.InnerDiameter * 0.98; % Example estimation
+
+            % The length of the axle could be estimated based on the length of the stator or rotor
+            axle.Length = stator.Length; % Example estimation
+
+            % Assign default or estimated values for material properties
+            axle.K_Conductivity = 50; % Default or estimated value
+            axle.C_HeatCapacity = 0.466; % Default or estimated value
+
+            % Inner diameter is typically zero for a solid axle
+            axle.InnerDiameter = 0;
+        end
+
+        function Q = estimateHeatTransfer(axle, stator, T_axle, T_stator)
+            % Axle and stator structures are expected to contain material properties and dimensions
+            % T_axle and T_stator are the temperatures of the axle and stator, respectively
+
+            % Calculate the cross-sectional area of the axle
+            A_cross_section = pi * (axle.OuterDiameter / 2)^2;
+
+            % Calculate the length of the heat path (assuming it's the length of the axle)
+            L_heat_path = axle.Length;
+
+            % Calculate the temperature gradient
+            delta_T = T_axle - T_stator;
+
+            % Fourier's law of heat conduction
+            Q = axle.K_Conductivity * A_cross_section * delta_T / L_heat_path;
+        end
+
+        function [motorSpeed, powerLoss] = estimateMotorParameters(V, I, Kv, R_phase)
+            % Estimate motor speed (RPM) assuming no load
+            motorSpeed = Kv * V;  % Ideal no-load speed in RPM
+
+            % Calculate power loss due to resistance in windings
+            powerLoss = I^2 * R_phase;  % Power loss in Watts
+        end
 
     end
 
