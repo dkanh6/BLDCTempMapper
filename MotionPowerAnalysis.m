5a60ded765d6f2068d145529c2a48e5448eb99b3
Daniel Amuedo <dkanh6@umsystem.edu>
2023-12-09 21:12:57 -0600

    Added Quadruped Wire Frame preview and helper picture

--- MotionPowerAnalysis.m
+++ MotionPowerAnalysis.m
@@ -2,19 +2,9 @@ classdef MotionPowerAnalysis
     methods (Static)
         % Function to plot a solid cylinder
         function plotSolidCylinder(ax, outerDiameter, innerDiameter, length, color, zOffset)
-            %disp(['Plotting cylinder with outer diameter: ', num2str(outerDiameter)]);
-            %disp(['Inner diameter: ', num2str(innerDiameter)]);
-            %disp(['Length: ', num2str(length)]);
-            %disp(['Color: ', color]);
-            %disp(['zOffset: ', num2str(zOffset)]);
 
             numPoints = 100; % Define the number of points on the circumference
             [X, Y, Z] = cylinder([outerDiameter/2, outerDiameter/2], numPoints);
-
-            % Debug: Size of the matrices X, Y, Z
-            %disp(['Size of X: ', mat2str(size(X))]);
-            %disp(['Size of Y: ', mat2str(size(Y))]);
-            %disp(['Size of Z: ', mat2str(size(Z))]);
 
             surf(ax, X, Y, Z*length + zOffset, 'FaceColor', color);
 
@@ -30,21 +20,118 @@ classdef MotionPowerAnalysis
             [XInnerCap, YInnerCap] = pol2cart(theta, innerDiameter/2);
 
             nCaps = size(XOuterCap, 2) - 1; % Number of segments for the cap
-            %disp(['Number of cap segments: ', num2str(nCaps)]);
 
             % Top cap
             for i = 1:nCaps
                 fill3(ax, [XOuterCap(i:i+1), XInnerCap(i+1:-1:i)], ...
-                      [YOuterCap(i:i+1), YInnerCap(i+1:-1:i)], ...
-                      [length + zOffset, length + zOffset, length + zOffset, length + zOffset], color);
+                    [YOuterCap(i:i+1), YInnerCap(i+1:-1:i)], ...
+                    [length + zOffset, length + zOffset, length + zOffset, length + zOffset], color);
             end
 
             % Bottom cap
             for i = 1:nCaps
                 fill3(ax, [XOuterCap(i:i+1), XInnerCap(i+1:-1:i)], ...
-                      [YOuterCap(i:i+1), YInnerCap(i+1:-1:i)], ...
-                      [zOffset, zOffset, zOffset, zOffset], color);
+                    [YOuterCap(i:i+1), YInnerCap(i+1:-1:i)], ...
+                    [zOffset, zOffset, zOffset, zOffset], color);
             end
+        end
+
+        function plotQuadruped(bodyLength, bodyWidth, bodyHeight, upperLegLength, lowerLegLength, shoulderAngles, kneeAngles)
+            % Verify that shoulderAngles and kneeAngles are 1x4 arrays
+            if length(shoulderAngles) ~= 4 || length(kneeAngles) ~= 4
+                error('Shoulder and knee angles must be 1x4 arrays.');
+            end
+
+            % Set the axes limits
+            maxDimension = max([bodyLength, bodyWidth, bodyHeight, upperLegLength, lowerLegLength]);
+            axisLimit = maxDimension * 1.0; % Adjust this factor as needed for the desired scale
+
+            % Define the body as a 3D box
+            bodyVertices = [-bodyLength/2, -bodyWidth/2, 0;
+                bodyLength/2, -bodyWidth/2, 0;
+                bodyLength/2,  bodyWidth/2, 0;
+                -bodyLength/2,  bodyWidth/2, 0;
+                -bodyLength/2, -bodyWidth/2, bodyHeight;
+                bodyLength/2, -bodyWidth/2, bodyHeight;
+                bodyLength/2,  bodyWidth/2, bodyHeight;
+                -bodyLength/2,  bodyWidth/2, bodyHeight];
+
+            bodyEdges = [1 2; 2 3; 3 4; 4 1; % Lower body square
+                5 6; 6 7; 7 8; 8 5; % Upper body square
+                1 5; 2 6; 3 7; 4 8]; % Body edges
+
+            % Create figure
+            figure;
+            hold on;
+            grid on;
+            axis equal;
+            view(25, 30);
+
+
+            xlim([-axisLimit, axisLimit]);
+            ylim([-axisLimit, axisLimit]);
+            zlim([-(2*upperLegLength), axisLimit]); % Assuming z starts from 0 (ground level)
+
+            % Define the radius of the spheres representing the joints
+            jointRadius = 0.025;
+
+            % Plot the body
+            for i = 1:size(bodyEdges, 1)
+                plot3(bodyVertices(bodyEdges(i,:), 1), ...
+                    bodyVertices(bodyEdges(i,:), 2), ...
+                    bodyVertices(bodyEdges(i,:), 3), 'b-','LineWidth',4);
+            end
+
+            % Define leg attachment points on the body
+            legAttachPoints = [-bodyLength/2, -bodyWidth/2, 0;
+                bodyLength/2, -bodyWidth/2, 0;
+                bodyLength/2,  bodyWidth/2, 0;
+                -bodyLength/2,  bodyWidth/2, 0];
+
+            % Plot the legs
+            for i = 1:4
+                % Convert angles from degrees to radians for MATLAB functions
+                shoulderAngleRad = deg2rad(shoulderAngles(i));
+                kneeAngleRad = deg2rad(kneeAngles(i));
+
+                % Upper leg segment (shoulder joint)
+                upperLegStart = legAttachPoints(i, :);
+                upperLegEnd = upperLegStart + ...
+                    [-upperLegLength * sin(shoulderAngleRad), 0, -upperLegLength * cos(shoulderAngleRad)];
+                plot3([upperLegStart(1), upperLegEnd(1)], ...
+                    [upperLegStart(2), upperLegEnd(2)], ...
+                    [upperLegStart(3), upperLegEnd(3)], 'k-','LineWidth',4);
+
+                % Lower leg segment (knee joint)
+                lowerLegStart = upperLegEnd;
+                lowerLegEnd = lowerLegStart + ...
+                    [-lowerLegLength * sin(kneeAngleRad), 0, -lowerLegLength * cos(kneeAngleRad)];
+                plot3([lowerLegStart(1), lowerLegEnd(1)], ...
+                    [lowerLegStart(2), lowerLegEnd(2)], ...
+                    [lowerLegStart(3), lowerLegEnd(3)], 'k-','LineWidth',4);
+
+                % Plot red sphere at the shoulder joint
+                [Xs, Ys, Zs] = sphere; % Generate sphere data
+                Xs = Xs * jointRadius + upperLegStart(1);
+                Ys = Ys * jointRadius + upperLegStart(2);
+                Zs = Zs * jointRadius + upperLegStart(3);
+                surf(Xs, Ys, Zs, 'EdgeColor', 'none', 'FaceColor', 'r');
+
+                % Plot red sphere at the knee joint
+                [Xk, Yk, Zk] = sphere; % Generate sphere data
+                Xk = Xk * jointRadius + lowerLegStart(1);
+                Yk = Yk * jointRadius + lowerLegStart(2);
+                Zk = Zk * jointRadius + lowerLegStart(3);
+                surf(Xk, Yk, Zk, 'EdgeColor', 'none', 'FaceColor', 'r');
+            end
+
+            % Set labels
+            xlabel('X');
+            ylabel('Y');
+            zlabel('Z');
+            title('3D Wireframe of a Quadruped Robot');
+
+            hold off;
         end
     end
 end
